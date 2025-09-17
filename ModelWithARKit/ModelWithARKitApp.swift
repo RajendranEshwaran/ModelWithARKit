973d3c8dbac65dafd47040c3ac26af49042e99a3
RAJENDRAN ESHWARAN <10699484+RajendranEshwaran@users.noreply.github.com>
2025-09-17 12:00:39 -0400

    Initial Commit

---
+++ ModelWithARKit/ModelWithARKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelWithARKitApp.swift
+//  ModelWithARKit
+//
+//  Created by RajayGoms on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ModelWithARKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
